4098d8bae7d499a69c8a6766502cb5beca6fb508
Andra Putra <andra0417@gmail.com>
2021-11-13 23:01:18 +0900

    Add button audio function, basic JS functions

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// Currently still uses global variables etc. Going to try to use callback functions in alpha branch.
+
+const pekolaAudio = document.getElementById("pekolaAudio");
+var count = 0;
+
+function htmlUpdater(htmlID, newContent) {
+    const element = document.getElementById(htmlID);
+    element.innerHTML = newContent;
+}
+
+function clickCounter() {
+    count += 1;
+    return count;
+}
+
+function pekoCounter() {
+    var currentCount = clickCounter();
+    htmlUpdater("sessionClicks", currentCount);    
+}
+
+// Will eventually play random peko clips out of array of like 3-5 random mp3 clips
+function pekoYell() {
+    playAudio(pekolaAudio);
+}
+
+function playAudio(audioFile) {
+    audioFile.play();
+}
+
+
+// Main button click function
 function pekoClick() {
-    console.log("Pek")
+    pekoYell();
+    pekoCounter();
+    
 }
