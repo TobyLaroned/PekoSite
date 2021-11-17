fb0b4989803814741b4eb025a1e2574fe0b37ae4
Andra Putra <andra0417@gmail.com>
2021-11-17 22:03:56 +0900

    Add getIndex function to play main soundfile 99% of time (changable)

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 // Messy solution, see if can integrate into single array
 
+const pekolaChance = 0.99; // Chance the main "itsamepekola" sound will play on button press
+
 const itsmepekola = new Audio("assets/audio/pekola.mp3");
 const ogeyyRrat = new Audio("assets/audio/ogeyyRrat.mp3");
 const okPeko = new Audio("assets/audio/okPeko.mp3");
@@ -20,14 +22,42 @@ function clickCounter() {
     return count;
 }
 
+// Outputs limited random choice based on wanted maximixed percentage mainPercent
+function getIndex(mainPercent, varAmount) {
+    var outputIndex = 0;
+    var d = Math.random();
+
+    if (varAmount == 1) {
+        return outputIndex;
+    } else if (d < mainPercent) {
+        return outputIndex; 
+    } else {
+        var percentLeftover = 1 - mainPercent;
+        var currentPercent = percentLeftover/(varAmount-1);
+        for (i = 0; i < varAmount-1; i++) {
+            // Repeat if check as many times as there are audio files
+            if ((d - mainPercent) < currentPercent) {
+                outputIndex += 1;
+                break;
+            } else {
+                outputIndex += 1;
+                currentPercent += percentLeftover/(varAmount-1); 
+            }
+        }
+        return outputIndex;
+    }
+}
+
 function pekoCounter() {
     var currentCount = clickCounter();
     htmlUpdater("sessionClicks", currentCount);    
 }
 
 function getRandomPekoSound() {
-    const random = Math.floor(Math.random() * pekolaAudios.length);
-    var audioOut = pekolaAudios[random];
+    // const random = Math.floor(Math.random() * pekolaAudios.length);
+    const audioIndex = getIndex(pekolaChance, pekolaAudios.length);
+    console.log(audioIndex);
+    var audioOut = pekolaAudios[audioIndex];
     return audioOut;
 }
 
@@ -38,14 +68,14 @@ function pekoYell(audioInput) {
 }
 
 // RAPID FIRE AUDIO PLAYER TRIAL FIX - WIP from stackoverflow
-function pekoYellerTest(){
-    var audio = document.createElement("audio");
-    audio.src = getRandomPekoSound();
-    audio.addEventListener("ended", function () {
-        document.removeChild(this);
-    }, false);
-    audio.play();   
-}
+// function pekoYellerTest(){
+//     var audio = document.createElement("audio");
+//     audio.src = getRandomPekoSound();
+//     audio.addEventListener("ended", function () {
+//         document.removeChild(this);
+//     }, false);
+//     audio.play();   
+// }
 
 
 // Main button click function
@@ -53,3 +83,6 @@ function pekoClick() {
     pekoYell(getRandomPekoSound());
     pekoCounter();    
 }
+
+
+
